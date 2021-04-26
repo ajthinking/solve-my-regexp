ebc791e32a2537a551e698f07ec2a8c791ce10c7
Anders Jurisoo <jurisoo@hotmail.com>
2021-04-26 15:24:42 +0200

    Restore basic app

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,13 +1,5 @@
 class ProblemsController < ApplicationController
   def index
     @problems = Problem.all
-    @hoho = 'hej'
-
-    a = 123
-    binding.pry
-    a = 456
-    binding.pry
-
-    
   end
 end
